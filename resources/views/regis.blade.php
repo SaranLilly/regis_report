961119c7d0ca3f585e9c0ae9b4b684cf283cdf75
Sunsakrit In Sand <89797155+guyiems@users.noreply.github.com>
2024-12-17 11:08:52 +0700

    regis_form

--- resources/views/regis.blade.php
+++ resources/views/regis.blade.php
@@ -4,52 +4,49 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>   
     <title>Register</title>
 </head>
 <body>
-    test
-    <template>
-        <v-sheet class="mx-auto" width="300">
-          <v-form fast-fail @submit.prevent>
-            <v-text-field
-              v-model="firstName"
-              :rules="firstNameRules"
-              label="First name"
-            ></v-text-field>
-      
-            <v-text-field
-              v-model="lastName"
-              :rules="lastNameRules"
-              label="Last name"
-            ></v-text-field>
-      
-            <v-btn class="mt-2" type="submit" block>Submit</v-btn>
-          </v-form>
-        </v-sheet>
-      </template>
-      <script>
-        export default {
-          data: () => ({
-            firstName: '',
-            firstNameRules: [
-              value => {
-                if (value?.length >= 3) return true
-      
-                return 'First name must be at least 3 characters.'
-              },
-            ],
-            lastName: '123',
-            lastNameRules: [
-              value => {
-                if (/[^0-9]/.test(value)) return true
-      
-                return 'Last name can not contain digits.'
-              },
-            ],
-          }),
-        }
-      </script>
+    <form>
+    <div class="container" style="width: 800px; margin-top: 70px">
+    <div class="text_head" style="text-align: center">
+       <h1> ลงทะเบียนร่วมกิจกรรมปีใหม่ </h1>  
+    </div>
+    <div class="foem_input">
+
     
+
+        <div class="mb-3">
+            <label for="exampleInputfname" class="form-label">ชื่อ</label>
+            <input type="fname" class="form-control" id="exampleInputfname" >
+          </div>
+
+          <div class="mb-3">
+            <label for="exampleInputlname" class="form-label">นามสกุล</label>
+            <input type="lname" class="form-control" id="exampleInputlname" >
+          </div>
+
+          <div class="mb-3">
+            <label for="exampleInputtel" class="form-label">เบอร์โทรศัพท์</label>
+            <input type="tel" class="form-control" id="exampleInputtel" >
+          </div>
+
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">อีเมล</label>
+          <input type="email" class="form-control" id="exampleInputEmail1" >
+        </div>
+
+        <div class="subminButton" style="Align=left" >
+            <p Align=right>
+             <button type="submit" class="btn btn-primary" >ลงทะเบียน</button>
+            </P>
+        </div>
+       
+      </form>
+    </div>
+</div>
 </body>
 </html>
